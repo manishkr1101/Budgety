501523ad83e3f4f809bc4b5fa25f324df7c6f656
manish <manishkumarsh120@gmail.com>
2019-12-10 22:31:35 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Budgety
+## This is an app for managing income and expenses in ordered way
---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+})
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log('server is running at localhost:3001');
+});
